1c13e759aa99f7cbeda9c70f77afaa6e61813b01
fuerteb <144563953+fuerteb@users.noreply.github.com>
2024-08-05 09:39:41 +0200

    Add files via upload

--- demo.js
+++ demo.js
@@ -20,24 +20,22 @@ function startConnect(){
         //userName: userId,
         //passwordId: passwordId
     });
-
-
 }
 
 
 function onConnect(){
-    topic =  "MINIS/Sommerfest"
+    topic =  "MINIS/Sub"
 
-    document.getElementById("messages").innerHTML += "<span> Subscribing to topic "+topic + "</span><br>";
+    //document.getElementById("messages").innerHTML += "<span> Subscribing to topic "+topic + "</span><br>";
 
-    client.subscribe(topic);
+    //client.subscribe(topic);
 
 
     // Resetten des Timers
     resetTimer();
     // Starten des Timers
     var display = document.getElementById("countdown"),
-        timer = 120; // Hier den Timerwert in Sekunden einstellen
+        timer = 600; // Hier den Timerwert in Sekunden einstellen
     startTimer(timer, display);
 }
 
@@ -52,7 +50,7 @@ function onConnectionLost(responseObject){
 
 function onMessageArrived(message){
     console.log("OnMessageArrived: "+message.payloadString);
-    document.getElementById("messages").innerHTML += "<span> Topic:"+message.destinationName+"| Message : "+message.payloadString + "</span><br>";
+    document.getElementById("messages").innerHTML += "Message received: \""+message.payloadString + "\" </span><br>";
 }
 
 function startDisconnect(){
@@ -61,68 +59,53 @@ function startDisconnect(){
     resetTimer();
 }
 
-function publishMessage(){
-msg = "Fließband start";
-topic = "MINIS/Sommerfest"
+function publishMessage(msg){
+//msg = "Fließband start";
+topic = "MINIS/Sub"
 
 Message = new Paho.MQTT.Message(msg);
 Message.destinationName = topic;
 
 client.send(Message);
-document.getElementById("messages").innerHTML += "<span> Message to topic "+topic+" is sent </span><br>";
-
-
+document.getElementById("messages").innerHTML += "<span> Message \"" + msg + "\" is sent </span><br>";
 }
 
-function publishMessage1(){
-    msg = "Fließband stop";
-    topic = "MINIS/Sommerfest"
-    
-    Message = new Paho.MQTT.Message(msg);
-    Message.destinationName = topic;
-    
-    client.send(Message);
-    document.getElementById("messages").innerHTML += "<span> Message to topic "+topic+" is sent </span><br>";
-    
-    
+var timerInterval;
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+
+    // Funktion zum Aktualisieren und Anzeigen der verbleibenden Zeit
+    function updateTimer() {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
     }
 
-    var timerInterval;
+    // Timer starten und das Interval-Objekt speichern
+    timerInterval = setInterval(function () {
+        updateTimer(); // Timer aktualisieren und anzeigen
 
-    function startTimer(duration, display) {
-        var timer = duration, minutes, seconds;
-    
-        // Funktion zum Aktualisieren und Anzeigen der verbleibenden Zeit
-        function updateTimer() {
-            minutes = parseInt(timer / 60, 10);
-            seconds = parseInt(timer % 60, 10);
-    
-            minutes = minutes < 10 ? "0" + minutes : minutes;
-            seconds = seconds < 10 ? "0" + seconds : seconds;
-    
-            display.textContent = minutes + ":" + seconds;
+        if (--timer < 0) {
+            clearInterval(timerInterval); // Timer stoppen, wenn er abgelaufen ist
+            // Aktion ausführen, wenn der Timer abgelaufen ist
+            startDisconnect();
         }
+    }, 1000);
+
+    // Timer anzeigen
+    updateTimer();
+}
     
-        // Timer starten und das Interval-Objekt speichern
-        timerInterval = setInterval(function () {
-            updateTimer(); // Timer aktualisieren und anzeigen
-    
-            if (--timer < 0) {
-                clearInterval(timerInterval); // Timer stoppen, wenn er abgelaufen ist
-                // Aktion ausführen, wenn der Timer abgelaufen ist
-                startDisconnect();
-            }
-        }, 1000);
-    
-        // Timer anzeigen
-        updateTimer();
-    }
-    
-    // Funktion zum Zurücksetzen des Timers
-    function resetTimer() {
-        clearInterval(timerInterval); // Timer stoppen
-        var display = document.getElementById("countdown");
-        display.textContent = "00:00"; // Timer-Anzeige zurücksetzen auf 10 Sekunden
-        //var timer = 10; // Timer zurücksetzen auf 10 Sekunden
-        //startTimer(timer, display); // Timer neu starten
-    }
+// Funktion zum Zurücksetzen des Timers
+function resetTimer() {
+    clearInterval(timerInterval); // Timer stoppen
+    var display = document.getElementById("countdown");
+    display.textContent = "00:00"; // Timer-Anzeige zurücksetzen auf 10 Sekunden
+    //var timer = 10; // Timer zurücksetzen auf 10 Sekunden
+    //startTimer(timer, display); // Timer neu starten
+}
